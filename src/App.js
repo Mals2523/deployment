ac823f639a8566ec06edeca61abb2ee0a9993c5c
Mals2523 <malathi.2523@gmail.com>
2025-02-09 00:31:42 +0530

    Fix: Added 404 page and improved navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import DoctorDashboard from "./pages/DoctorDashboard";
-import PatientHistory from "./pages/Patient_History"; 
+import PatientHistory from "./pages/Patient_History";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Default homepage route */}
+        <Route path="/" element={<DoctorDashboard />} />
+
+        {/* Other routes */}
         <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
         <Route path="/patient-history" element={<PatientHistory />} />
+
+        {/* 404 Fallback Route */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
+  );
+}
+
+// Separate 404 component for better styling
+function NotFound() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <a href="#/">Go to Home</a>
+    </div>
   );
 }
 
